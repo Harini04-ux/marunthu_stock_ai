61cf85092d8e5847b7e1ad72c5223c9c3d7ae04b
Keerthana-DSMAX <keerthysekar195@gmail.com>
2026-08-21 15:10:35 +0530

    Connect database using Render DATABASE_URL

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:marunthu@localhost:5432/marunthu_stock"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
